6e99243a019224bee1d863c3a6b994c0e85e8ac6
Steve Klabnik <steve@steveklabnik.com>
2019-05-31 11:33:56 -0500

    I know how to write javascript

--- worker/worker/worker.js
+++ worker/worker/worker.js
@@ -50,7 +50,7 @@ function normalize_path(path) {
 
 function is_directory(path) {
   const bits = path.split("/")
-  const last = bits.last()
+  const last = bits[bits.length -1];
 
   // does the final component contain a dot? technically there may be edge cases
   // here but this is fine for now!
