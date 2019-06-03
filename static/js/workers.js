3727b54e617d5dc0f6edefd80985a3f621e8f5a6
Victoria <bernard.victoria22@gmail.com>
2019-06-03 13:45:32 -0500

    Added copy paste to wrangler commands

---
+++ static/js/workers.js
@@ -0,0 +1,36 @@
+function selectText(node) {
+  var selection = window.getSelection()
+  var range = document.createRange()
+  range.selectNodeContents(node)
+  selection.removeAllRanges()
+  selection.addRange(range)
+  return selection
+}
+
+function addCopyButton(containerEl) {
+  let copyBtn = document.createElement('div')
+  copyBtn.className = 'help'
+  copyBtn.textContent = 'Click to Copy'
+  let lastEl = containerEl.querySelector('.highlight')
+  containerEl.insertBefore(copyBtn, lastEl)
+
+  copyBtn.addEventListener('click', function(el) {
+    const target = el.target
+    let highlight = el.target.parentElement.querySelector('.highlight')
+    let text = selectText(highlight)
+    document.execCommand('copy')
+    text.removeAllRanges()
+    el.target.innerText = 'Copied to Clipboard'
+
+    setTimeout(() => {
+      target.innerText = 'Click to Copy'
+    }, 6000)
+  })
+}
+
+window.addEventListener('DOMContentLoaded', event => {
+  let highlightBlocks = document.querySelectorAll('.copy')
+  Array.from(highlightBlocks).forEach(element => {
+    addCopyButton(element)
+  })
+})
