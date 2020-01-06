c282e60edb811a5afbd86adf6bfe3745376928eb
Victoria <bernard.victoria22@gmail.com>
2020-01-06 14:53:30 -0600

    filler pages to tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,14 @@ import Image from '../components/image'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+  <>
     <h1>Hi people</h1>
     <p>Index.js insrc</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </>
 )
 
 export default IndexPage
