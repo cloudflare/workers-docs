82ba9243f7c473b1aff99ad3330d9172f5de01e7
Victoria <bernard.victoria22@gmail.com>
2020-01-08 15:32:57 -0600

    Clean up unused imports

--- src/templates/baseTemplate.tsx
+++ src/templates/baseTemplate.tsx
@@ -2,11 +2,8 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-import '../static/css/main.css'
-import '../static/css/overview.css'
-import '../static/css/code.css'
-import '../static/css/template.css'
-import { markdownRemarkResult, markdownPageContext, markdownRemark } from '../types/markdownRemark'
+
+import { markdownRemarkResult, markdownPageContext } from '../types/markdownRemark'
 import Body from '../components/Body'
 
 type markdownBaseProps = {
@@ -15,9 +12,7 @@ type markdownBaseProps = {
 }
 const Template: React.FC<markdownBaseProps> = ({
   data, // this prop will be injected by the GraphQL query below.
-  pageContext,
-  children
-
+  pageContext
 }) => {
   console.log('pageContext', pageContext)
   const { markdownRemark } = data // data.markdownRemark holds our post data
@@ -56,19 +51,3 @@ export const pageQuery = graphql`
     }
   }
 `
-const mark: markdownRemark = {
-  "html": "<p>These templates are simple building blocks for developing Workers scripts.</p>\n<div style=\"display: flex\">\n  <div style=\"flex: 2; margin-right: 16px;\">\n    <label style=\"font-weight: normal; color: #666;\">Search templates</label>\n    <input id=\"search\" placeholder=\"🔎 Search by template name or other details\" style=\"padding: 10px; width: 100%\"></input>\n  </div>\n  <div style=\"flex: 1; margin-right: 16px;\">\n    <label style=\"font-weight: normal; color: #666;\">Type</label>\n    <select id=\"type\" style=\"width: 100%\">\n      <option>All</option>\n      <option>Boilerplates</option>\n      <option>Snippets</option>\n      <option value=\"featured_boilerplates\">Featured</option>\n    </select>\n  </div>\n</div>\n<div id=\"empty\" style=\"display: none; margin-top: 20px;\">\n<p>No results were found for your search. Try adjusting your search.</p>\n</div>\n<p>{{<gallery>}}</p>\n<p>The gallery is actively growing. The <a href=\"https://github.com/victoriabernard92/workers-template-creator\">template creator</a> allows you to share templates. Host a public repo, and then run <code>wrangler generate <a href=\"https://github.com/\">https://github.com/</a><your-repo></code>.</p>\n<p>For archived recipes, see <a href=\"https://developers.cloudflare.com/workers/recipes/\">the old docs</a>.</p>\n</div>\n<script src=\"https://unpkg.com/lunr/lunr.js\"></script>\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.15/lodash.min.js\" integrity=\"sha256-VeNaFBVDhoX3H+gJ37DpT/nTuZTdjYro9yBruHjVmoQ=\" crossorigin=\"anonymous\"></script>\n<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css\">\n<script src=\"https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js\"></script>\n<script src=\"/js/templates.js\"></script>",
-  "fields": {
-    "filePath": "/templates/_index.md",
-    "parent": "/",
-    "pathToServe": "/workers/templates"
-  },
-  "fileAbsolutePath": "/Users/victoriabernard/my-repos/gatsby-site/src/markdown-pages/templates/_index.md",
-  "frontmatter": {
-    "title": "Template Gallery",
-    "weight": 2,
-    "showNew": null,
-    "hidden": null,
-    "alwaysopen": true
-  }
-}
--- src/templates/templatePage.tsx
+++ src/templates/templatePage.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { graphql, Link } from 'gatsby'
-import { Helmet } from "react-helmet";
+import React, { } from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-import '../static/css/main.css'
-import '../static/css/overview.css'
-import '../static/css/code.css'
-import '../static/css/template.css'
+
 import Body from '../components/Body'
 import TemplatePage from '../components/TemplatePage';
-import { allRestApiTemplates, restApiTemplate, restApiTemplateResult } from '../types/restApiTemplates';
+import { restApiTemplateResult } from '../types/restApiTemplates';
 type templateProps = {
   // id: string
   pageContext: any
@@ -62,29 +58,3 @@ export const pageQuery = graphql`query ($id: String!) {
 }
 }`
 
-const blah: restApiTemplateResult = {
-  "data": {
-    "restApiTemplates": {
-      "repository_url": null,
-      "share_url": "/templates/snippets/ab_testing",
-      "weight": 1,
-      "url": null,
-      "tags": null,
-      "title": "A/B Testing",
-      "endpointId": "ab_testing",
-      "description": "Set up an A/B test by controlling what response is served based on cookies",
-      "type": "snippet",
-      "code": "async function handleRequest(request) {\n  const NAME = 'experiment-0'\n  // Responses below are place holders, you could set up\n  // a custom path for each test (e.g. /control/somepath )\n  const TEST_RESPONSE = new Response('Test group') // fetch('/test/sompath', request)\n  const CONTROL_RESPONSE = new Response('Control group') // fetch('/control/sompath', request)\n  // Determine which group this requester is in.\n  const cookie = request.headers.get('cookie')\n  if (cookie && cookie.includes(`${NAME}=control`)) {\n    return CONTROL_RESPONSE\n  } else if (cookie && cookie.includes(`${NAME}=test`)) {\n    return TEST_RESPONSE\n  } else {\n    // if no cookie then this is a new client, decide a group and set the cookie\n    let group = Math.random() < 0.5 ? 'test' : 'control' // 50/50 split\n    let response = group === 'control' ? CONTROL_RESPONSE : TEST_RESPONSE\n    response.headers.append('Set-Cookie', `${NAME}=${group}; path=/`)\n    return response\n  }\n}\naddEventListener('fetch', event => {\n  event.respondWith(handleRequest(event.request))\n})\n",
-      "demos": {
-        "bar": null,
-        "foo": null,
-        "main": {
-          "share_url": null,
-          "tags": null,
-          "text": "Demo",
-          "url": "https://cloudflareworkers.com/#12a2962014e1324e3a416a5a76e1f81f:https://tutorial.cloudflareworkers.com"
-        }
-      }
-    }
-  }
-}
