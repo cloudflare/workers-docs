8846bbcd2025849ffdc383b86c18e44ab57b8316
Victoria <bernard.victoria22@gmail.com>
2020-01-16 15:24:06 -0600

    Manually set prefixs in JSX or with  MDX

--- src/components/Boilerplate.tsx
+++ src/components/Boilerplate.tsx
@@ -2,6 +2,7 @@ import { restApiTemplate } from "../types/restApiTemplates"
 import React from 'react'
 import { Link } from "gatsby"
 import { useRestApiTemplates } from "../hooks/useMarkdownRemark"
+import { PREFIX } from "./utils"
 type boilerplateProps = restApiTemplate & {
   page_url?: string
 }
@@ -16,8 +17,10 @@ export const Boilerplate: React.FC<boilerplateProps> = (props) => {
   }
   let { endpointId, description, title, share_url, tags, repository_url } = props.description ? props : getBoilerplate(props.endpointId || "")
   let { page_url } = props
-  const template_page = "/workers/templates/pages/" + endpointId
-  page_url = share_url || template_page // TODO may need to consider tutorial? 
+  const template_page = "/templates/pages/" + endpointId
+  page_url = share_url ? ("/" + share_url) : template_page // TODO may need to consider tutorial? // TODO may need to consider tutorial? 
+  // TODO use regex to make sure leading slash
+  page_url = PREFIX + page_url
   return (<figure className="template-card boilerplate" id="{{.id}}">
     <div className="tag-group">
       {tags?.map(tag => (
