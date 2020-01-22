dffd7859fd83e12af77ec1a8549284833733b6f8
Victoria <bernard.victoria22@gmail.com>
2020-01-22 17:49:12 -0600

    Works through hardcoding prefix, messy fix me

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,14 @@ import { useStaticQuery, graphql } from 'gatsby'
 
 import Sidebar from './Sidebar'
 import SEO from './Seo'
-
+// import "../content/static"
+// import "../content/static/js/docsearch.min.js"
+// import "/static/js/workers.js"
+import '../content/static/css/theme.css'
+import '../content/static/css/main.css'
+import '../content/static/css/overview.css'
+import '../content/static/css/code.css'
+import '../content/static/css/template.css'
 import { Navigation } from './Navigation'
 const Layout: React.FC<LayoutProps> = ({ title, children }) => {
   // TODO get hooks working instead of useStaticQuery in components
