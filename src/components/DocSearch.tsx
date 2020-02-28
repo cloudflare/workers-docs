efe443ca24f06ecedad7519202c89f6a86309e11
Victoria <Bernard.victoria22@gmail.com>
2020-02-28 15:26:38 -0600

    Fix doc search

--- src/components/DocSearch.tsx
+++ src/components/DocSearch.tsx
@@ -1,16 +1,16 @@
 import React, { FunctionComponent, useEffect } from 'react'
 
-const DocSearch: FunctionComponent<{ initial?: boolean }> = ({ initial = true }) => {
+const DocSearch: FunctionComponent<{ initial?: boolean }> = ({
+  initial = true,
+}) => {
   // const [searchLoadedState, setSearchState] = useState(initial)
   useEffect(() => {
-    console.log('as')
     // @ts-ignore
     if (window.docsearch) {
-      console.log('docsearch did load')
       // @ts-ignore
       window.docsearch({
-        apiKey: '4c1a7e1b6289032a8e8fd1dbbae112a3',
-        indexName: 'cloudflare',
+        apiKey: '4a3bbdb939606486b94f9ce867bfd4f5',
+        indexName: 'workers-cloudflare',
         inputSelector: '#docsearch-input',
       })
     } else {
