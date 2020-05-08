627cb80b830b4ae99cd9a4b52581389d192bfa5f
Victoria <Bernard.victoria22@gmail.com>
2020-05-08 17:22:42 -0500

    typescript setup

--- cloudflare-docs-theme/gatsby-config.js
+++ cloudflare-docs-theme/gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = (themeOptions) => {
         options: {
           name: `mdx-pages`,
           path: contentPath,
-          ignore: [`**/CONTRIBUTING*`, '/styles/**', 'index.js'],
+          ignore: [`**/CONTRIBUTING*`, 'styles/**', 'index.js'],
         },
       },
       {
@@ -29,36 +29,36 @@ module.exports = (themeOptions) => {
         },
       },
 
-      ...require('glob')
-        // TODO: instead of serving images this complicated way, change links to root
-        // e.g. (/tooling/media/image.jpq) to ref current directory (e.g. ./media/image.jpg)
-        .sync(path.join(__dirname, './src/static'))
-        .map((source) => {
-          // console.log('path.join', path.join(__dirname, './src'))
-          const destination = path.replace(path.join(__dirname, './src/content'), '')
-          return {
-            resolve: 'gatsby-plugin-copy-files',
-            options: {
-              source,
-              destination: destination,
-            },
-          }
-        }),
-      ...require('glob')
-        // TODO: instead of serving images this complicated way, change links to root
-        // e.g. (/tooling/media/image.jpg) to ref current directory (e.g. ./media/image.jpg)
-        // this place all images under media directoy into their current folder under content for gatsby
-        // then gatsby figured out who to put it in current folder at public/workers/..
-        .sync(path.join(__dirname, './src/**/media'))
-        .map((source) => {
-          return {
-            resolve: 'gatsby-plugin-copy-files',
-            options: {
-              source,
-              destination: source.replace(path.join(__dirname, './src/content'), publicPath),
-            },
-          }
-        }),
+      // ...require('glob')
+      //   // TODO: instead of serving images this complicated way, change links to root
+      //   // e.g. (/tooling/media/image.jpq) to ref current directory (e.g. ./media/image.jpg)
+      //   .sync(path.join(__dirname, './src/static'))
+      //   .map((source) => {
+      //     // console.log('path.join', path.join(__dirname, './src'))
+      //     const destination = path.replace(path.join(__dirname, './src/content'), '')
+      //     return {
+      //       resolve: 'gatsby-plugin-copy-files',
+      //       options: {
+      //         source,
+      //         destination: destination,
+      //       },
+      //     }
+      //   }),
+      // ...require('glob')
+      //   // TODO: instead of serving images this complicated way, change links to root
+      //   // e.g. (/tooling/media/image.jpg) to ref current directory (e.g. ./media/image.jpg)
+      //   // this place all images under media directoy into their current folder under content for gatsby
+      //   // then gatsby figured out who to put it in current folder at public/workers/..
+      //   .sync(path.join(__dirname, './src/**/media'))
+      //   .map((source) => {
+      //     return {
+      //       resolve: 'gatsby-plugin-copy-files',
+      //       options: {
+      //         source,
+      //         destination: source.replace(path.join(__dirname, './src/content'), publicPath),
+      //       },
+      //     }
+      //   }),
     ],
   }
 }
